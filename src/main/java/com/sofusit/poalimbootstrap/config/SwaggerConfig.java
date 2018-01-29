d24278e41039c7450c461c1ca5872a1bbc106667
Tal Etinger <etingertal@gmail.com>
2018-01-29 13:38:33 +0200

    Added some swagger info

--- src/main/java/com/sofusit/poalimbootstrap/config/SwaggerConfig.java
+++ src/main/java/com/sofusit/poalimbootstrap/config/SwaggerConfig.java
@@ -3,7 +3,10 @@ package com.sofusit.poalimbootstrap.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,9 +20,22 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
                 .select()
                 .apis(withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring REST Sample with Swagger")
+                .description("Spring REST Sample with Swagger")
+                .termsOfServiceUrl("http://www.google.com/")
+                .contact(new Contact("Tal Etinger", "", "etingertal@gmail.com"))
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://github.com/IBM-Bluemix/news-aggregator/blob/master/LICENSE")
+                .version("2.0")
                 .build();
     }
 }
--- src/main/java/com/sofusit/poalimbootstrap/controller/SignController.java
+++ src/main/java/com/sofusit/poalimbootstrap/controller/SignController.java
@@ -5,6 +5,7 @@ import com.sofusit.poalimbootstrap.excpetion.ResourceNotFoundException;
 import com.sofusit.poalimbootstrap.model.SignRequest;
 import com.sofusit.poalimbootstrap.model.SignResponse;
 import com.sofusit.poalimbootstrap.service.SignService;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -19,8 +20,20 @@ public class SignController {
     SignService signService;
 
     // Get specific sign
+    @ApiOperation(value = "getSign", nickname = "getSign")
     @RequestMapping(value = "/{signId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<SignResponse> getSign(@PathVariable("signId") String signId) throws ResourceNotFoundException {
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "signId", value = "Sign's id", required = true, dataType = "string")
+    })
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success", response = SignResponse.class),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Failure")})
+    public ResponseEntity<SignResponse> getSign(@PathVariable("signId") String signId)
+            throws ResourceNotFoundException {
+
         Sign sign = signService.findById(signId);
 
         return new ResponseEntity<>(new SignResponse(sign), HttpStatus.OK);
